8c8f58da46f4b2f2340160c2fda44b1b056d2ede
Cameron Morrison <cmorrison@idea11.com.au>
2022-11-02 14:46:20 +1000

    remove starship installer, copy colours into place

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,13 @@ sudo apt update
 sudo apt install exa 
 
 # starship
-curl -sS https://starship.rs/install.sh > install.sh 
-sh ./install.sh --yes
+curl -sS https://starship.rs/install.sh > install-starship.sh 
+sh ./install-starship.sh --yes
+rm install-starship.sh
 
 # configure zsh
+mkdir -p ~/.config/zsh
+cp .config/zsh/lscolours.sh ~/.config/zsh/lscolours.sh
 cp .zshrc ~/.zshrc
 sudo chsh -s $(which zsh) $(whoami)
 
